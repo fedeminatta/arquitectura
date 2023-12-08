ce3b9ddcba82d9a3bbd6cff93939968f06e7d890
fedeminatta <minattafederico@gmail.com>
2023-12-08 11:54:03 -0300

    fade

--- src/components/ListProyects/ListProyectsSlice.tsx
+++ src/components/ListProyects/ListProyectsSlice.tsx
@@ -1,19 +1,21 @@
 import proyectosArray from '../../utils/proyectos';
 import Proyects from '../Proyects/Proyects';
-import { Slide } from 'react-awesome-reveal';
+import { Fade } from 'react-awesome-reveal';
 import styles from './ListProyects.module.scss';
 
 const ListProyectsSlice = () => {
   return (
     <section className={styles.proyectos}>
-      {proyectosArray.slice(0, 2).map((item) => (
-        <Proyects
-          key={item.titulo}
-          title={item.titulo}
-          description={item.description}
-          img={item.img}
-        />
-      ))}
+      <Fade triggerOnce direction="right" cascade>
+        {proyectosArray.slice(0, 2).map((item) => (
+          <Proyects
+            key={item.titulo}
+            title={item.titulo}
+            description={item.description}
+            img={item.img}
+          />
+        ))}
+      </Fade>
     </section>
   );
 };
