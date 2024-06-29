82c08165f48651359eaa6d652ee336ec9b7f0c18
fedeminatta <minattafederico@gmail.com>
2024-06-28 22:25:13 -0300

    estilos

--- src/components/SectionInfo/SectionInfo.tsx
+++ src/components/SectionInfo/SectionInfo.tsx
@@ -6,17 +6,22 @@ interface Props {
   description: string;
   img: any;
   right?: boolean;
+  bgColor?: string;
 }
-const SectionInfo = ({ title, description, img, right }: Props) => {
+const SectionInfo = ({ title, description, img, right, bgColor }: Props) => {
   return (
     <Fade triggerOnce direction={right ? 'right' : 'left'}>
-      <section className={styles.section} id="acerca_de">
+      <section
+        className={styles.section}
+        id="acerca_de"
+        style={{ backgroundColor: bgColor }}
+      >
         <div className={styles.img}>
           <img src={img.src} alt={title} width={350} />
         </div>
         <article
           className={styles.article}
-          style={{ order: right ? '-1' : '1' }}
+          style={{ backgroundColor: 'transparent', order: right ? '-1' : '1' }}
         >
           <h2>{title}</h2>
           <p>{description}</p>
