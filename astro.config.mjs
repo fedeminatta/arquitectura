158dceba64ae52b5263719be64091525a32d2221
fedeminatta <minattafederico@gmail.com>
2025-08-14 11:35:40 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({}),
+  adapter: vercel(),
   integrations: [react(), tailwind()],
   alias: {
     '@components': '/src/components/',
