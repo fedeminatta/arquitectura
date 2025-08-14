45b42e6a7fafbfd66efb0e1ce80005186a5e0557
fedeminatta <minattafederico@gmail.com>
2025-08-14 12:17:19 -0300

    s

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BclQiymy.mjs';
-import { manifest } from './manifest_CQLprai4.mjs';
+import { manifest } from './manifest_Dw88-NsJ.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "2d3bcf1a-a70b-45a2-b38b-5e06a814b3f5",
+    "middlewareSecret": "52fe0f2e-9f5b-4ac5-b43d-ccd1621be834",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    functionPerRoute: false
+  }),
   integrations: [react(), tailwind()],
   alias: {
     '@components': '/src/components/',
